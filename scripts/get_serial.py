e2ecf17809ec534c1e6498dec8837d66b23cb6b5
clickok <babennet@ualberta.ca>
2015-07-21 17:15:08 -0600

    Added script for getting serial

---
+++ scripts/get_serial.py
@@ -0,0 +1,16 @@
+"""
+Get the serial number associated with a Raspberry Pi in a mildly kludge-y way.
+"""
+
+
+def main():
+    try:
+        for line in open('/proc/cpuinfo', 'r').readlines():
+            if line[0:6] == "Serial":
+                return line[10:].strip()
+    except Exception as e:
+        raise(e)
+
+if __name__ == "__main__":
+    main()
+
