7408059678ec7a4e092009ae509ba675547f32d5
rldotai <babennett@protonmail.com>
2017-01-19 19:29:42 -0700

    Reorganization

--- apt_python3.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt-get install -y python3 
-sudo apt-get install -y python3-numpy python3-serial python3-rpi.gpio ipython3
--- testing_commands.sh
+++
@@ -1,6 +0,0 @@
-
-# Install relevant items for playing video
-sudo apt-get install mplayer 
-
-# Play webcam to monitor
-mplayer tv:///
